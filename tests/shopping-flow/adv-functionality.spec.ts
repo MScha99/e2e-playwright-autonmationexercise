747463240938806e842ea85b6d4949b798b02127
Hakkene <dogier2006@gmail.com>
2025-05-07 13:03:29 +0200

    tc-020 fix

--- tests/shopping-flow/adv-functionality.spec.ts
+++ tests/shopping-flow/adv-functionality.spec.ts
@@ -3,10 +3,15 @@ import { AppUrls } from '../../config/urls'
 import { HeaderComponent } from '../../components/header.component'
 import { ProductsPage } from '../../pages/products.page'
 import { ProductDetailsPage } from '../../pages/product-details.page'
-import { products } from '../../config/test_data'
+import {
+  products,
+  AccountInformation,
+  generateUniqueEmail,
+} from '../../config/test_data'
 import { HomePage } from '../../pages/home.page'
 import { CartPage } from '../../pages/cart.page'
 import { CartModalComponent } from '../../components/cart-modal.component'
+import { deleteUserAccount, createUserAccount } from '../../utils/accountUtils'
 
 test.beforeEach(async ({ page }) => {
   await page.goto(AppUrls.BASE_URL)
@@ -18,6 +23,8 @@ test('TC-020 Add to cart from recommended items on homepage', async ({
   const productsPage = new ProductsPage(page)
   const cartPage = new CartPage(page)
   const headerComponent = new HeaderComponent(page)
+  const itemsAddedToCart: string[] = []
+  const uniqueEmail = generateUniqueEmail()
 
   await test.step('Search for product on All Products page, and verify search results', async () => {
     await headerComponent.productPageLink.click()
@@ -30,7 +37,6 @@ test('TC-020 Add to cart from recommended items on homepage', async ({
 
   await test.step('Add all products in search result to cart, and verify cart contents ', async () => {
     const searchResultCount = await productsPage.singleProductCell.count()
-    const itemsAddedToCart: string[] = []
 
     for (let i = 0; i < searchResultCount; i++) {
       const { description } = await productsPage.addNthItemToCart(i)
@@ -47,6 +53,51 @@ test('TC-020 Add to cart from recommended items on homepage', async ({
       expect(await cartPage.verifyItemsExistInCart(desc)).toBeTruthy()
     }
   })
+  await test.step('Create account, then login to account with correct credentials', async () => {
+    const signupLoginPage = await headerComponent.openSignupLoginPage()
 
-  await test.step('Login to account with correct credentials, then go back to cart and verify its contents again', async () => {})
+    await createUserAccount(
+      page,
+      AccountInformation.valid.name,
+      uniqueEmail,
+      AccountInformation.valid.title,
+      // AccountInformation.valid.name,
+      AccountInformation.valid.password,
+      AccountInformation.valid.dateOfBirth.day,
+      AccountInformation.valid.dateOfBirth.month,
+      AccountInformation.valid.dateOfBirth.year,
+      AccountInformation.valid.newsletter,
+      AccountInformation.valid.specialOffers,
+      AccountInformation.valid.address.firstName,
+      AccountInformation.valid.address.lastName,
+      AccountInformation.valid.address.company,
+      AccountInformation.valid.address.address1,
+      AccountInformation.valid.address.address2,
+      AccountInformation.valid.address.country,
+      AccountInformation.valid.address.state,
+      AccountInformation.valid.address.city,
+      AccountInformation.valid.address.zipcode,
+      AccountInformation.valid.address.mobileNumber
+    )
+    await page.goto(AppUrls.BASE_URL)
+    await headerComponent.logoutButton.click()
+    await expect(signupLoginPage.loginHeading).toBeVisible()
+    await signupLoginPage.fillLoginFormAndSubmit(
+      uniqueEmail,
+      AccountInformation.valid.password
+    )
+    await expect(headerComponent.loggedInAsUsername).toBeVisible()
+  })
+  await test.step(' go back to cart and verify its contents again', async () => {
+    await headerComponent.cartPageLink.click()
+    for (const desc of itemsAddedToCart) {
+      expect(await cartPage.verifyItemsExistInCart(desc)).toBeTruthy()
+    }
+
+    await headerComponent.deleteAccountButton.click()
+  })
 })
+
+// test('TC-020 Search products', async ({ page }) => {
+
+// })
